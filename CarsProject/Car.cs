96112c655582c15ffc0733dcbb60c6be132231cc
eisaman <emi18@pitt.edu>
2023-01-23 20:27:42 -0500

    first version with main menu
    adds cars but doesn't delete or use menus right

---
+++ CarsProject/Car.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace CarsProject 
+{
+    public class Car
+    {
+        public int year;
+        public string? make;
+        public string? model;
+        public int NumberOfDoors;
+        public double price;
+    }
+}
